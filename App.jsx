185bd050e30f6ddf4e140269f035ee857166696d
m0hammadzaib <zaibfr4@gmail.com>
2025-11-12 14:40:12 +0530

    Switching to Tailwind Css

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import "./global.css";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-800">
+      <Text className="text-2xl font-bold text-red-500">Tailwind Works</Text>
+    </View>
+  );
+}
